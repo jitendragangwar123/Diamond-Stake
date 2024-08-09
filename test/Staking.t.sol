8486e9e0ad91a03c350ba0d0ad79f1ea01dbc281
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-08-09 23:11:50 +0530

    feat:test-case for staking ether

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -4,4 +4,33 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Staking} from "../src/Staking.sol";
 
-contract StakingTest is Test {}
+contract StakingTest is Test {
+    Staking staking;
+    address user1;
+    address user2;
+    uint256 constant DAY = 1 days;
+
+    function setUp() public {
+        staking = new Staking();
+        user1 = address(0x123);
+        user2 = address(0x456);
+        vm.deal(user1, 1 ether);
+        vm.deal(user2, 1 ether);
+    }
+
+    function testStakeEther() public {
+        vm.startPrank(user1);
+        uint256 numDays = 30;
+        uint256 stakeAmount = 0.1 ether;
+
+        uint256 initialBalance = user1.balance;
+        staking.stakeEther{value: stakeAmount}(numDays);
+
+        Staking.Position memory position = staking.getPositionById(0);
+        assertEq(position.walletAddress, user1);
+        assertEq(position.weiStaked, stakeAmount);
+        assertEq(position.unlockDate, block.timestamp + (numDays * DAY));
+        assertTrue(position.open);
+        assertEq(user1.balance, initialBalance - stakeAmount);
+    }
+}
